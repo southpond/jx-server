8022858ed8e547c2334a5ac63320c0ed902abd1d
gewei <1804005113@qq.cm>
2020-01-06 18:15:00 +0800

    2020年1月6日18点14分 package-lock.json--M package.json--M baseComponent.js--prototype--M v1.js--routes--M

--- routes/v1.js
+++ routes/v1.js
@@ -2,12 +2,15 @@ import express from "express";
 import CityHandle from "../controller/v1/cities";
 import Captchas from "../controller/v1/captchas";
 import SearchPlace from "../controller/v1/search";
+import BaseComponent from "../prototype/baseComponent";
 
+const baseHandle = new BaseComponent();
 const router = express.Router();
 
 router.get("/cities", CityHandle.getCity);
 router.get("/cities/:id", CityHandle.getCityById);
 router.post("/captchas", Captchas.getCaptchas);
 router.get("/pois", SearchPlace.search);
+router.post("/addimg", baseHandle.uploadImg);
 
 export default router;
