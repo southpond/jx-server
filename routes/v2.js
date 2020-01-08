4331e7848938a9e3754a7178d1a33bd94df9fb51
gewei <1804005113@qq.cm>
2020-01-08 18:27:07 +0800

    2020年1月8日18点26分 entry.js--controller/v2--U entry.js--models/v2--U v2.js--routes--M

--- routes/v2.js
+++ routes/v2.js
@@ -1,8 +1,10 @@
 import express from "express";
-import User from '../controller/v2/user'
+import User from "../controller/v2/user";
+import Entry from "../controller/v2/entry";
 
 const router = express.Router();
 
-router.post('/login', User.login);
+router.get("/index_entry", Entry.getEntry);
+router.post("/login", User.login);
 
-export default router
+export default router;
